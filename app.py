08b964574bf851a562c3f8538c1d8d2fb9c996c1
Aryan Raj <aryanraj2713@gmail.com>
2024-01-23 14:09:48 +0530

    api_debug

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import numpy as np
 from PIL import Image
 from io import BytesIO
 
-
-
 app = Flask(__name__)
 
 def apply_blur(image, face_coordinates):
@@ -15,19 +13,14 @@ def apply_blur(image, face_coordinates):
         image[y:y+roi.shape[0], x:x+roi.shape[1]] = roi
     return image
 
-
-
 def detect_faces(image):
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
     face_coordinates = face_cascade.detectMultiScale(image, 1.3, 5)
     return face_coordinates
 
-
-
 def read_imagefile(file) -> np.ndarray:
     image = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_COLOR)
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
 
 @app.route('/blur-faces', methods=['POST'])
 def blur_faces():
@@ -35,27 +28,24 @@ def blur_faces():
         file = request.files['file']
         image = read_imagefile(file)
         face_coordinates = detect_faces(image)
-        
-
-
         if len(face_coordinates) > 0:
             image = apply_blur(image, face_coordinates)
             image_pil = Image.fromarray(image)
             img_byte_array = BytesIO()
             image_pil.save(img_byte_array, format="PNG")
-
             return send_file(BytesIO(img_byte_array.getvalue()), mimetype='image/png')
         else:
             return jsonify({'error': 'No faces detected in the image'}), 400
-
-
-
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
 
 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
 
 
 # if __name__ == '__main__':
@@ -63,5 +53,5 @@ def blur_faces():
 
 
 
-if __name__ == 'app.py':
-    app.run(debug=True)
+# if __name__ == 'app.py':
+#     app.run(debug=True)
